fd5b68afd2a1dd0222b147d22e62053456fa933e
Gene Hightower <gene@digilicious.com>
2015-12-22 12:34:51 -0800

    C++ style ptr

--- hbxx.hpp
+++ hbxx.hpp
@@ -9,11 +9,15 @@ namespace hbxx {
 
 class buffer {
 public:
-  buffer() : buf_(CHECK_NOTNULL(hb_buffer_create())) {}
+  buffer()
+    : buf_(CHECK_NOTNULL(hb_buffer_create()))
+  {
+  }
 
   ~buffer() { hb_buffer_destroy(buf_); }
 
-  void add_utf8(std::string const &str) {
+  void add_utf8(std::string const &str)
+  {
     hb_buffer_add_utf8(buf_, str.data(), str.length(), 0, str.length());
   }
 
